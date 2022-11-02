b4090e4086bd68a14c6817845b7c78f95efb1ba9
thanhlongst2020 <68694926+thanhlongst2020@users.noreply.github.com>
2022-11-02 11:07:46 +0700

    Re-Init FrontEnd Project

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+
+import Home from "./components/Home";
+import Cart from "./components/Cart";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header/>
+      <div>
+        <Route exact path='/'>
+          <Home/>
+        </Route>
+        <Route exact path='/cart'>
+          <Cart/>
+        </Route>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
